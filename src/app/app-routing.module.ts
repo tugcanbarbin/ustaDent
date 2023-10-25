6b7b4fe8e3c795fb999cc12f6b26c20e5505f856
tugcanbarbin <tugcanbarbin@hotmail.com>
2023-10-25 18:58:29 +0300

    first css import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: '/home', pathMatch: 'full',  title: "Demo site" },
+  { path: 'home', component: HomepageComponent ,  title: "Demo site" },
+  // { path: 'about', component: AboutusComponent ,  title: "Demo site" },
+  // { path: 'services', component: ServicesComponent ,  title: "Demo site" },
+  // { path: 'contact', component: ContactComponent,  title: "Demo site"  },
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
